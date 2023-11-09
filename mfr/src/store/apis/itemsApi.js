0509d3e80f97ff3b6c333685ce5b38f7ba349a7d
Zacknetic <zackavino@gmail.com>
2023-11-09 12:12:02 -0500

    Modified host to be accessible outside of the local machine
    Modified itemsApi, luggagesApi, tripsApi to use the local machines IP. This is an example and will no longer be valid if the local machine's IP changes.
    
    Modified package.json to host the backend server on the same IP address. Again, this must be changed if the machine's IP changes or if it is hosted on another device (with a different IP)

--- mfr/src/store/apis/itemsApi.js
+++ mfr/src/store/apis/itemsApi.js
@@ -1,64 +1,61 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { faker } from '@faker-js/faker';
 
 const itemsApi = createApi({
-    reducerPath: 'items',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3005'
-    }),
-    endpoints(builder) {
-        return {
-            fetchItems: builder.query({
-                providesTags: (result, error, luggage) => {
-                    const tags = result.map((item) => {
-                        return { type: 'Item', id: item.id}
-                    });
-                    tags.push({ type: 'LuggagesItem', id: luggage.id});
-                    return tags;
-                },
-                query: (luggage) => {
-                    return {
-                        url: '/items',
-                        params: {
-                            luggageId: luggage.luggageId
-                        },
-                        method: 'GET'
-                    }
-                }                               
-            }),
-            addItem: builder.mutation({
-                invalidatesTags: (result, error, luggage) => {
-                    return [{ type: 'LuggagesItem', id: luggage.id}]
-                },
-                query: (luggage) => {
-                    return {
-                        method: 'POST',
-                        url: '/items',
-                        body: {
-                            luggageId: luggage.id,
-                            entry: faker.commerce.productName()
-                        }
-                    }
-                }
-            }),
-            removeItem: builder.mutation({
-                invalidatesTags: (result, error, item) => {
-                    return [{ type: 'Item', id: item.id}]
-                },
-                query: (item) => {
-                    return {
-                        method: 'DELETE',
-                        url: `/items/${item.id}`
-                    };
-                },
-            }),
-        };
-    }
+	reducerPath: 'items',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://192.168.50.179:3005', //this must change if your IP changes
+	}),
+	endpoints(builder) {
+		return {
+			fetchItems: builder.query({
+				providesTags: (result, error, luggage) => {
+					const tags = result.map((item) => {
+						return { type: 'Item', id: item.id };
+					});
+					tags.push({ type: 'LuggagesItem', id: luggage.id });
+					return tags;
+				},
+				query: (luggage) => {
+					return {
+						url: '/items',
+						params: {
+							luggageId: luggage.luggageId,
+						},
+						method: 'GET',
+					};
+				},
+			}),
+			addItem: builder.mutation({
+				invalidatesTags: (result, error, luggage) => {
+					return [{ type: 'LuggagesItem', id: luggage.id }];
+				},
+				query: (luggage) => {
+					return {
+						method: 'POST',
+						url: '/items',
+						body: {
+							luggageId: luggage.id,
+							entry: faker.commerce.productName(),
+						},
+					};
+				},
+			}),
+			removeItem: builder.mutation({
+				invalidatesTags: (result, error, item) => {
+					return [{ type: 'Item', id: item.id }];
+				},
+				query: (item) => {
+					return {
+						method: 'DELETE',
+						url: `/items/${item.id}`,
+					};
+				},
+			}),
+		};
+	},
 });
 
-export const {
-    useFetchItemsQuery,
-    useAddItemMutation,
-    useRemoveItemMutation
-} = itemsApi;
+export const { useFetchItemsQuery, useAddItemMutation, useRemoveItemMutation } =
+	itemsApi;
 export { itemsApi };
--- mfr/src/store/apis/luggagesApi.js
+++ mfr/src/store/apis/luggagesApi.js
@@ -3,74 +3,74 @@ import { faker } from '@faker-js/faker';
 
 // DEV ONLY!!!
 const pause = (duration) => {
-    return new Promise((resolve) => {
-      setTimeout(resolve, duration);
-    });
-  };
+	return new Promise((resolve) => {
+		setTimeout(resolve, duration);
+	});
+};
 
 const luggagesApi = createApi({
-    reducerPath: 'luggages',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3005',
-        fetchFn: async(...args) => {
-            //REMOVE FOR PRODUCTION. ONLY FOR TEST
-            await pause(1000);
-            return fetch(...args);
-        }
-    }),
-    endpoints(builder) {
-        return {
-            fetchLuggage: builder.query({
-                providesTags: (result, error, trip) => {
-                    const tags = result.map(luggage => {
-                        return { type:'Luggage', id: luggage.id}
-                    });
-                    tags.push({type: 'TripsLuggage', id: trip.id});
-                    return tags;
-                },
-                query: (trip) => {
-                    return {
-                        url: '/luggages',
-                        params: {
-                            tripId: trip.id,
-                        },
-                        method: 'GET'
-                    }
-                }
-            }),
-            addLuggage: builder.mutation({
-                invalidatesTags: (result, error, trip) => {
-                    return [{ type: 'TripsLuggage', id: trip.id }]
-                },
-                query: (trip) => {
-                    return {
-                        url: 'luggages',
-                        method: 'POST',
-                        body: {
-                            tripId: trip.id,
-                            title: faker.commerce.productName()
-                        }
-                    }
-                }
-            }),
-            removeLuggage: builder.mutation({
-                invalidatesTags: (result, error, luggage) => {
-                    return [{ type: 'Luggage', id: luggage.id }]
-                },
-                query: (luggage) => {
-                    return {
-                        url: `/luggages/${luggage.id}`,
-                        method: 'DELETE',
-                    }
-                }
-            })
-        };
-    }
+	reducerPath: 'luggages',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'http://192.168.50.179:3005', //this must change if your IP changes
+		fetchFn: async (...args) => {
+			//REMOVE FOR PRODUCTION. ONLY FOR TEST
+			// await pause(1000); //removing this as we no longer want to simulate delays
+			return fetch(...args);
+		},
+	}),
+	endpoints(builder) {
+		return {
+			fetchLuggage: builder.query({
+				providesTags: (result, error, trip) => {
+					const tags = result.map((luggage) => {
+						return { type: 'Luggage', id: luggage.id };
+					});
+					tags.push({ type: 'TripsLuggage', id: trip.id });
+					return tags;
+				},
+				query: (trip) => {
+					return {
+						url: '/luggages',
+						params: {
+							tripId: trip.id,
+						},
+						method: 'GET',
+					};
+				},
+			}),
+			addLuggage: builder.mutation({
+				invalidatesTags: (result, error, trip) => {
+					return [{ type: 'TripsLuggage', id: trip.id }];
+				},
+				query: (trip) => {
+					return {
+						url: 'luggages',
+						method: 'POST',
+						body: {
+							tripId: trip.id,
+							title: faker.commerce.productName(),
+						},
+					};
+				},
+			}),
+			removeLuggage: builder.mutation({
+				invalidatesTags: (result, error, luggage) => {
+					return [{ type: 'Luggage', id: luggage.id }];
+				},
+				query: (luggage) => {
+					return {
+						url: `/luggages/${luggage.id}`,
+						method: 'DELETE',
+					};
+				},
+			}),
+		};
+	},
 });
 
-export const { 
-    useFetchLuggageQuery, 
-    useAddLuggageMutation, 
-    useRemoveLuggageMutation 
+export const {
+	useFetchLuggageQuery,
+	useAddLuggageMutation,
+	useRemoveLuggageMutation,
 } = luggagesApi;
 export { luggagesApi };
